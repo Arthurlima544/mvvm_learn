106413289591bccac2c90cf86e4e834ec5bde1eb
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-08 15:34:12 -0300

    feat: login and logout View model

---
+++ lib/ui/auth/login/view_models/login_viewmodel.dart
@@ -0,0 +1,29 @@
+import 'package:logging/logging.dart';
+import 'package:mvvm_learn/data/repositories/auth/auth_repository.dart';
+import 'package:mvvm_learn/utils/command.dart';
+import 'package:mvvm_learn/utils/result.dart';
+
+class LoginViewmodel {
+  LoginViewmodel({required AuthRepository authRepository})
+    : _authRepository = authRepository {
+    login = Command1<void, (String email, String password)>(_login);
+  }
+
+  final AuthRepository _authRepository;
+  final _log = Logger('LoginViewModel');
+
+  late Command1 login;
+
+  Future<Result<void>> _login((String, String) credentials) async {
+    final (email, password) = credentials;
+    final result = await _authRepository.login(
+      email: email,
+      password: password,
+    );
+
+    if (result is Error<void>) {
+      _log.warning('Login failed! ${result.error}');
+    }
+    return result;
+  }
+}
---
+++ lib/ui/auth/logout/view_models/logout_viewmodel.dart
@@ -0,0 +1,32 @@
+import 'package:mvvm_learn/data/repositories/auth/auth_repository.dart';
+import 'package:mvvm_learn/data/repositories/itinerary_config/itinerary_config_repository.dart';
+import 'package:mvvm_learn/domain/models/itinerary_config/itinerary_config.dart';
+
+import '../../../../utils/command.dart';
+import '../../../../utils/result.dart';
+
+class LogoutViewModel {
+  LogoutViewModel({
+    required AuthRepository authRepository,
+    required ItineraryConfigRepository itineraryConfigRepository,
+  }) : _authLogoutRepository = authRepository,
+       _itineraryConfigRepository = itineraryConfigRepository {
+    logout = Command0(_logout);
+  }
+  final AuthRepository _authLogoutRepository;
+  final ItineraryConfigRepository _itineraryConfigRepository;
+  late Command0 logout;
+
+  Future<Result> _logout() async {
+    final result = await _authLogoutRepository.logout();
+
+    switch (result) {
+      case Ok<void>():
+        return _itineraryConfigRepository.setItineraryConfig(
+          const ItineraryConfig(),
+        );
+      case Error<void>():
+        return result;
+    }
+  }
+}
